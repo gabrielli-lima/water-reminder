88148af4a66286893c9628d9a48e83d2e826abcb
gabrielli-lima <gabriellilima165@gmail.com>
2023-09-09 17:39:43 -0300

    feat: add modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,16 +1,23 @@
+import glassImage from "../assets/glass.svg";
+
 type Props = {
-    isOpen: boolean;
-}
+  isOpen: boolean;
+};
 
-export const Modal = (props : Props) =>{
-    const isOpen = props.isOpen
-    if(isOpen===true){
-        return(
-            <div className="bg-orange-500" w-96 h-96>foiiii</div>
-        )
-    } else{
-        return(<div className="bg-gray-500" w-96 h-96>não foi 222</div>)
-    }
-
-    
-}
+export const Modal = (props: Props) => {
+  const isOpen = props.isOpen;
+  if (isOpen) {
+    return (
+      <div className="absolute inset-0 m-auto bg-[#1D1B26] w-[300px] h-[255px] bg-fixed rounded-3xl flex justify-center items-center gap-6">
+        <div className="flex flex-col justify-center">
+            <div className="absolute top-0 right-0 bg-[#322F40] m-3 rounded-full w-8 h-8 text-[16px] mx-3 text-white text-center flex justify-center items-center">X</div>
+            <img src={glassImage} alt="" className="w-[40px] h-[62px]" />
+          <h1 className="text-center text-[#7FC4ED] text-2xl font-semibold">
+            Lembrete para beber água!
+          </h1>
+        </div>
+      </div>
+    );
+  }
+  return null;
+};
