a34ab850721acd77fc3649c0d16cd62cc6d00d13
gabrielli-lima <gabriellilima165@gmail.com>
2023-09-11 23:07:19 -0300

    feat: add modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import waterImage from "./assets/water.png";
 import glassImage from "./assets/glass.svg";
 import { useState } from "react";
-import { Modal } from "./components/Modal";
+import CustomModal from "./components/Modal";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false);
 
-  const handleButtonClick = () => {
-    setIsModalOpen(true)
-  }
-
+  const handleModalOpen = () => setModalOpen(true);
+  const handleClose = () => setModalOpen(false);
 
   return (
     <div className="container mx-auto w-4/5 mt-5">
@@ -25,11 +23,7 @@ function App() {
       <div className="md:grid md:grid-cols-3 md:gap-12">
         <aside className="md:grid w-52 h-[350px] m-auto bg-[#7FC4ED] rounded-[20px] text-center grid content-center">
           <span className="text-xs font-bold">80%</span>
-          <img
-            src={glassImage}
-            alt=""
-            className="mx-auto mt-8 mb-10"
-          />
+          <img src={glassImage} alt="" className="mx-auto mt-8 mb-10" />
           <div className="flex flex-col">
             <span className="text-xl font-semibold">Beber água</span>
             <span className="text-base font-medium">Meta: 3L</span>
@@ -41,40 +35,25 @@ function App() {
             <span className="flex justify-between items-center text-base font-medium">
               Meta diária <span className="text-xs text-[#4E4964]">300ml</span>
             </span>
-
-            <input
-              className="mt-2"
-              type="range"
-              name="metaDiaria"
-              id="metaDiaria"
-              min="0"
-              max="300"
-            />
+            <input className="mt-2 in-range:border-green-500" type="range" name="metaDiaria" id="metaDiaria" min="0" max="300"  />
           </label>
           <label className="grid text-[#7FC4ED] mt-7">
             <span className="flex justify-between items-center text-base font-medium">
               Quantidade por timer
               <span className="text-xs text-[#4E4964]">300ml</span>
             </span>
-
-            <input
-              className="mt-2"
-              type="range"
-              name="qntdPorTimer"
-              id="qntdPorTimer"
-              min="0"
-              max="300"
-            />
+            <input className="mt-2" type="range" name="qntdPorTimer" id="qntdPorTimer" min="0" max="300" />
           </label>
           <div className="my-7 flex justify-center items-center ">
-                <div className="w-16 h-16 bg-[#322F40] text-white text-[32px] text-center font-normal flex justify-center items-center rounded-lg leading-none" x-text="hours">00</div>
-                <div className="text-xs text-[#4E4964] mx-1">h</div>
-                <div className="text-[32px] mx-3 text-white">:</div>
-                <div className="w-16 h-16 bg-[#322F40] text-white text-[32px] text-center font-normal flex justify-center items-center rounded-lg leading-none" x-text="minutes">00</div>
-                <div className="text-xs text-[#4E4964] mx-1">m</div>
+            <div className="w-16 h-16 bg-[#322F40] text-white text-[32px] text-center font-normal flex justify-center items-center rounded-lg leading-none" x-text="hours">00</div>
+            <div className="text-xs text-[#4E4964] mx-1">h</div>
+            <div className="text-[32px] mx-3 text-white">:</div>
+            <div className="w-16 h-16 bg-[#322F40] text-white text-[32px] text-center font-normal flex justify-center items-center rounded-lg leading-none" x-text="minutes">00</div>
+            <div className="text-xs text-[#4E4964] mx-1">m</div>
           </div>
-          <button type="button" className="bg-[#7FC4ED] text-base text-black font-medium w-full p-4 rounded-md" onClick={handleButtonClick}>Começar</button>
-          {isModalOpen && <Modal isOpen={true} /> }
+          <CustomModal open={modalOpen} onClose={handleClose} />
+          <button type="button" className="bg-[#7FC4ED] text-base text-black font-medium w-full p-4 rounded-md" onClick={handleModalOpen}>Começar</button>
+
         </section>
       </div>
     </div>
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,23 +1,42 @@
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
 import glassImage from "../assets/glass.svg";
 
-type Props = {
-  isOpen: boolean;
+type ModalProps = {
+  open: boolean;
+  onClose: () => void;
 };
 
-export const Modal = (props: Props) => {
-  const isOpen = props.isOpen;
-  if (isOpen) {
-    return (
-      <div className="absolute inset-0 m-auto bg-[#1D1B26] w-[300px] h-[255px] bg-fixed rounded-3xl flex justify-center items-center gap-6">
-        <div className="flex flex-col justify-center">
-            <div className="absolute top-0 right-0 bg-[#322F40] m-3 rounded-full w-8 h-8 text-[16px] mx-3 text-white text-center flex justify-center items-center">X</div>
+export default function CustomModal({ open, onClose }: ModalProps) {
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={onClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box className="absolute inset-0 m-auto bg-[#1D1B26] w-[300px] h-[255px] bg-fixed rounded-3xl flex flex-col justify-center items-center gap-6 px-12">
+            <div className="absolute top-0 right-0 bg-[#322F40] m-3 rounded-full w-8 h-8 text-[16px] mx-3 text-white text-center flex justify-center items-center">
+              X
+            </div>
             <img src={glassImage} alt="" className="w-[40px] h-[62px]" />
-          <h1 className="text-center text-[#7FC4ED] text-2xl font-semibold">
-            Lembrete para beber água!
-          </h1>
-        </div>
-      </div>
-    );
-  }
-  return null;
-};
+            <h1 className="text-center text-[#7FC4ED] text-2xl font-semibold">
+              Lembrete para beber água!
+            </h1>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
